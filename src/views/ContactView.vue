ebd5e5466b7983384fea95ebf33b86daa6b7fc90
cata <kat.espinosa.m@gmail.com>
2024-10-23 02:11:54 -0300

    first commit

---
+++ src/views/ContactView.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+  <section class="container px-5">
+    <div class="row">
+      <div class="col pt-5">
+        <h1 class="text-center pb-3">Contactame</h1>
+        <p class="d-none d-md-block text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+          sunt quia nobis tempora aliquam, labore similique sapiente ut
+          praesentium veniam fuga quae vitae error. Facilis commodi repellat eum
+          nesciunt quas?
+        </p>
+
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label"
+            >Nombre</label
+          >
+          <input
+            type="text"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="Nombre"
+          />
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label"
+            >Correo Electrónico</label
+          >
+          <input
+            type="email"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name@example.com"
+          />
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label"
+            >Mensaje</label
+          >
+          <textarea
+            class="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
+        </div>
+        <div class="mb-5">
+          <button
+            type="button"
+            class="ms-auto text-end btn btn-info text-white"
+          >
+            Enviar
+          </button>
+        </div>
+      </div>
+    </div>
+    <button @click="VolverAlInicio" class="btn btn-primary">Ir a Inicio</button>
+  </section>
+</template>
+<script>
+export default {
+  methods: {
+    VolverAlInicio() {
+      this.$router.push({ name: "inicio" });
+    },
+  },
+};
+</script>
---
+++ src/views/ProductosView.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="container" id="productos">
+    <h1 class="text-center">Productos</h1>
+    <div class="row">
+      <div class="col-sm-4 col-12 px-5 mb-4">
+        <SingleCard
+          :nombre="nombre"
+          :descripcion="descripcion"
+          :precio="precio"
+          :cantidad="cantidad"
+          :imagen="imagen"
+        />
+      </div>
+    </div>
+
+    <div>
+      <button @click="VolverAlInicio" class="btn btn-primary">
+        Ir a Inicio
+      </button>
+    </div>
+  </section>
+</template>
+<script>
+import SingleCard from "../components/SingleCard.vue";
+
+export default {
+  methods: {
+    VolverAlInicio() {
+      this.$router.push({ name: "inicio" });
+    },
+  },
+  components: {
+    SingleCard,
+  },
+  props: {
+    // productos: Array,
+    require: true,
+    nombre: {
+      type: String,
+      required: true,
+    },
+    descripcion: {
+      type: String,
+      required: true,
+    },
+    precio: {
+      type: Number,
+      required: true,
+    },
+    cantidad: {
+      type: String,
+      required: true,
+    },
+    imagen: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
